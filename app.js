33903ec6dc5b54401e0e7117ae44d5068c9f02b4
Ur <g.ulzi94@yahoo.com>
2020-01-16 00:33:58 +0800

    File uurchluv

--- app.js
+++ app.js
@@ -3,11 +3,68 @@ var activePlayer = 1;
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч 
 var scores = [0, 0]; // 2 тоглогчийн оноог массив хэлбэрээр зарлав.
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+
 var roundScore = 0;
-
 // Шооны аль талаараа буусныг хадгалах хувьсагч, 1 - 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = 2;
 
-console.log("Шоо : " + dice + " талаараа буулаа.");
-//
-//
+// <div class="player-score" id="score-0">43</div>
+
+// document.querySelector('#score-0').textContent = diceNumber;
+
+
+// Prepare the start
+
+document.getElementById('score-0').textContent = "0";
+document.getElementById('score-1').textContent = "0";
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";
+document.querySelector(".dice").style.display = "none";
+
+function beginStart(){
+document.getElementById('score-0').textContent = "0";
+document.getElementById('score-1').textContent = "0";
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";
+document.querySelector(".dice").style.display = "none";
+}
+
+// Event listener
+document.querySelector(".btn-roll").addEventListener("click", shooShid);
+document.querySelector(".btn-new").addEventListener("click", beginStart);
+document.querySelector(".btn-hold").addEventListener("click", storeNumber);
+
+var i = 0;
+function shooShid(){
+    var diceNumber = Math.floor(Math.random()*6) + 1;
+    document.querySelector(".dice").style.display = "block";
+    document.querySelector(".dice").src = "dice-" + diceNumber + ".png";
+    // 
+    if (diceNumber !== 1){
+        roundScore += diceNumber;
+        //document.getElementById("current-1").textContent = roundScore;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    }
+    else {
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        document.getElementById("score-" + activePlayer).textContent = 0;
+        roundScore = 0;
+        // document.querySelector(".dice").style.display = "none";
+        swap();
+    }
+        // alert("Шоо " + diceNumber + " талаараа буулаа.");
+}
+function storeNumber(){
+    scores[activePlayer] += roundScore;
+    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+    roundScore = 0;
+    swap();
+    }
+
+   function swap(){
+    if (activePlayer === 1 ) {
+        activePlayer = 0;
+    }
+    else {
+        activePlayer = 1;
+    }
+   }
