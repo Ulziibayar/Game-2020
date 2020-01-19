30b9298a42b325f6037f191e33e67054be4b4ca9
Ur <g.ulzi94@yahoo.com>
2020-01-19 20:17:31 +0800

    app uurchlugdsun

--- app.js
+++ app.js
@@ -1,81 +1,74 @@
-// Тоглогчийн ээлжийг хадгалах хувьсагч нэгдүгээр тоглогч - 0, хоёрдугаар тоглогч - 1
-function beginStart(){}
-var activePlayer = 1;
+var activePlayer = 0;
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч 
 var scores = [0, 0]; // 2 тоглогчийн оноог массив хэлбэрээр зарлав.
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-
 var roundScore = 0;
-// Шооны аль талаараа буусныг хадгалах хувьсагч, 1 - 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+// Event listener
+document.querySelector(".btn-roll").addEventListener("click", shooShid);
+document.querySelector(".btn-new").addEventListener("click", beginStart);
+document.querySelector(".btn-hold").addEventListener("click", storeNumber);
 
-// <div class="player-score" id="score-0">43</div>
+function shooShid(){
+
+    var diceNumber = Math.floor(Math.random()*6) + 1;
+    document.querySelector(".dice").style.display = "block";
+    document.querySelector(".dice").src = "dice-" + diceNumber + ".png";
+    // 
+    if (diceNumber !== 1){
+        roundScore += diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    }
+    else {
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        roundScore = 0;
+        swap();
+    }
+    }
+function storeNumber(){
+    scores[activePlayer] += roundScore;
+    if (scores[activePlayer] <= 100) {
+    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    roundScore = 0;
+    swap();
+    }
+    else{
+    document.getElementById("name-" + activePlayer).textContent = "WINNER !!!";
+    document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+    document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+    }
+    }
+
+   function swap(){
+    if (activePlayer === 1 ) {
+        document.querySelector(".player-" + activePlayer + "-panel").classList.toggle("active");
+        activePlayer = 0;
+        document.querySelector(".player-" + activePlayer + "-panel").classList.toggle("active");
+        //document.querySelector(".dice").style.display = "none";
+        }
+    else {
+        document.querySelector(".player-" + activePlayer + "-panel").classList.toggle("active");
+        activePlayer = 1;
+        document.querySelector(".player-" + activePlayer + "-panel").classList.toggle("active");
+        //document.querySelector(".dice").style.display = "none";
+    }
+   }
+
+   function beginStart(){
+    activePlayer = 1
+    // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч 
+    scores = [0, 0]; // 2 тоглогчийн оноог массив хэлбэрээр зарлав.
+    // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+    roundScore = 0;
 
 // document.querySelector('#score-0').textContent = diceNumber;
-
-
 // Prepare the start
-
 document.getElementById('score-0').textContent = "0";
 document.getElementById('score-1').textContent = "0";
 document.getElementById('current-0').textContent = "0";
 document.getElementById('current-1').textContent = "0";
 document.querySelector(".dice").style.display = "none";
 
-// 
-// document.getElementById('score-0').textContent = "0";
-// document.getElementById('score-1').textContent = "0";
-// document.getElementById('current-0').textContent = "0";
-// document.getElementById('current-1').textContent = "0";
-// document.querySelector(".dice").style.display = "none";
-// roundScore = 0;
-// }
-
-// Event listener
-document.querySelector(".btn-roll").addEventListener("click", shooShid);
-document.querySelector(".btn-new").addEventListener("click", beginStart);
-document.querySelector(".btn-hold").addEventListener("click", storeNumber);
-
-var i = 0;
-function shooShid(){
-
-    var diceNumber = Math.floor(Math.random()*6) + 1;
-    document.querySelector(".dice").style.display = "block";
-    document.querySelector(".dice").src = "dice-" + diceNumber + ".png";
-    
-    // 
-    if (diceNumber !== 1){
-        roundScore += diceNumber;
-        //document.getElementById("current-1").textContent = roundScore;
-        document.getElementById("current-" + activePlayer).textContent = roundScore;
-    }
-    else {
-        document.getElementById("current-" + activePlayer).textContent = 0;
-        document.getElementById("score-" + activePlayer).textContent = 0;
-        roundScore = 0;
-        // document.querySelector(".dice").style.display = "none";
-        swap();
-    }
-        // alert("Шоо " + diceNumber + " талаараа буулаа.");
-
-}
-function storeNumber(){
-    scores[activePlayer] += roundScore;
-    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
-    roundScore = 0;
-    swap();
-    }
-
-   function swap(){
-    if (activePlayer === 1 ) {
-       
-        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
-        activePlayer = 0;
-        document.querySelector(".player-" + activePlayer + "-panel").classList.add("active");
-        }
-    else {
-        
-        document.querySelector(".player-" + activePlayer + "-panel").classList.add("active");
-        activePlayer = 1;
-        
-    }
+document.getElementById("name-0").textContent = "Player 1";
+document.getElementById("name-1").textContent = "Player 2";
    }
